7ef4b4b9c2c1ea27bf02510f1d73bec06c006336
moaberglund <moa.21@hotmail.com>
2024-05-13 12:57:06 +0200

    angular material set up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { CoursesComponent } from './courses/courses.component';
 import { FrameworkComponent } from './framework/framework.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { CourseTableComponent } from './course-table/course-table.component';
 
 export const routes: Routes = [
     {path: "home", component: HomeComponent },
-    {path: "courses", component: CoursesComponent},
+    {path: "courses", component: CourseTableComponent},
     {path: "framework", component: FrameworkComponent},
     {path: "", redirectTo: "/home", pathMatch: "full"},
     {path: "404", component: NotFoundComponent},
---
+++ src/app/course-table/course-table.component.ts
@@ -0,0 +1,28 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTableModule, MatTable } from '@angular/material/table';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { CourseTableDataSource, CourseTableItem } from './course-table-datasource';
+
+@Component({
+  selector: 'app-course-table',
+  templateUrl: './course-table.component.html',
+  styleUrl: './course-table.component.scss',
+  standalone: true,
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule]
+})
+export class CourseTableComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<CourseTableItem>;
+  dataSource = new CourseTableDataSource();
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
