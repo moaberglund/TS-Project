26b23a9620c9693c0427d79fe27c8f07acda0533
moaberglund <moa.21@hotmail.com>
2024-05-13 12:42:58 +0200

    interface kurs

---
+++ src/app/models/course.ts
@@ -0,0 +1,12 @@
+export interface Course {
+    courseCode: String,
+    subjectCode: String,
+    level: String,
+    progression: String,
+    courseName:String,
+    points: Number,
+    institutionCode: String,
+    subject: String,
+    syllabus: String
+  
+}
