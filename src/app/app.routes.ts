a53ae5e9994522ab9c365c2bc04cdb2b4452fe1d
moaberglund <moa.21@hotmail.com>
2024-05-13 11:13:38 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FrameworkComponent } from './framework/framework.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component: HomeComponent },
+    {path: "courses", component: CoursesComponent},
+    {path: "framework", component: FrameworkComponent},
+    {path: "", redirectTo: "/home", pathMatch: "full"},
+    {path: "404", component: NotFoundComponent},
+    {path:"**", component: NotFoundComponent} //plockar upp allt annat och re-route till 404-sidan
+];
