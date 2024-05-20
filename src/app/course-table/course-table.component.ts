477bdc741104f9cd1b2aa98d79167c082d940a93
moaberglund <moa.21@hotmail.com>
2024-05-20 13:17:07 +0200

    funktionallitet för sökning aka filtrering

--- src/app/course-table/course-table.component.ts
+++ src/app/course-table/course-table.component.ts
@@ -1,18 +1,19 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatTableModule, MatTable } from '@angular/material/table';
+import { MatTableModule, MatTable, MatTableDataSource } from '@angular/material/table';
 import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 import { MatSortModule, MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { CourseTableDataSource } from './course-table-datasource';
 import { CourseItem } from '../models/course';
 import { AllCoursesService } from '../services/all-courses.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-course-table',
   templateUrl: './course-table.component.html',
   styleUrl: './course-table.component.scss',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule]
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule, MatInputModule]
 })
 export class CourseTableComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -35,14 +36,21 @@ export class CourseTableComponent implements AfterViewInit {
   private loadData(): void {
     this.allCoursesService.getCourses().subscribe(
       (data: CourseItem[]) => {
-      this.dataSource.data = data;
-      if (this.paginator) {
-        this.paginator.length = data.length;
-      }
-    },
-  error => {
-    console.error("Error fetching data from API", error);
-  });
+        this.dataSource.data = data;
+        if (this.paginator) {
+          this.paginator.length = data.length;
+        }
+      },
+      error => {
+        console.error("Error fetching data from API", error);
+      });
 
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.paginator.firstPage(); // Återställ pagineringen till första sidan
+  }
+
 }
