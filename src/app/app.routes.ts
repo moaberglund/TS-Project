154ebb799fe55c941ca87e44585cea7a05b7d884
moaberglund <moa.21@hotmail.com>
2024-05-20 12:13:59 +0200

    re-routing via courses componenten istället

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FrameworkComponent } from './framework/framework.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { CourseTableComponent } from './course-table/course-table.component';
+import { CoursesComponent } from './courses/courses.component';
 
 export const routes: Routes = [
     {path: "home", component: HomeComponent },
-    {path: "courses", component: CourseTableComponent},
+    {path: "courses", component: CoursesComponent},
     {path: "framework", component: FrameworkComponent},
     {path: "", redirectTo: "/home", pathMatch: "full"},
     {path: "404", component: NotFoundComponent},
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CourseTableComponent } from '../course-table/course-table.component';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [CourseTableComponent],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
