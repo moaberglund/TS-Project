46849ccbc288803af221cf16b3f4dfe7c127d26c
moaberglund <moa.21@hotmail.com>
2024-05-21 08:32:55 +0200

    add och get funktioner för localstorage

--- src/app/services/framework.service.ts
+++ src/app/services/framework.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { CourseItem } from '../models/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FrameworkService {
 
+  private storageKey = 'savedCourses'; //nyckel för att spara kurser
+
   constructor() { }
+
+  // Lägg till en kurs till localStorage
+  addCourse(course: CourseItem): void {
+    const courses = this.getCourses();  //hämta de befintliga kurserna från localstorage
+    courses.push(course);  //Lägger till ny kurs
+    localStorage.setItem(this.storageKey, JSON.stringify(courses));  //sparar den uppdaterade listan till localstorage
+  }
+
+  // Hämta alla sparade kurser från localStorage
+  getCourses(): CourseItem[] {
+    const savedCourses = localStorage.getItem(this.storageKey);   //hämta från localstorage
+    return savedCourses ? JSON.parse(savedCourses) : [];  //om det finns sparade kurser, returnera som en array, annars en tom array
+  }
+
+
 }
