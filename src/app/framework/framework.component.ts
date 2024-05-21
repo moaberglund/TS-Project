358a0292a1253fe4ce46a475cff33e9d092f8a52
moaberglund <moa.21@hotmail.com>
2024-05-21 10:03:27 +0200

    försök på ramshema sidan, fungerar inte helt ännu

--- src/app/framework/framework.component.ts
+++ src/app/framework/framework.component.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CourseItem } from '../models/course';
 import { FrameworkService } from '../services/framework.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-framework',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatSelectModule, 
+    MatIconModule, 
+    MatPaginatorModule, 
+    MatSortModule],
   templateUrl: './framework.component.html',
   styleUrl: './framework.component.scss'
 })
-export class FrameworkComponent implements OnInit {
+export class FrameworkComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ["courseCode", "courseName", "points", "subject", "syllabus", "remove"];
 
-  savedCourses: CourseItem[] = [];
+  savedCourses = new MatTableDataSource<CourseItem>(); // Lägg till ? för att markera att den kan vara undefined
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private frameworkService: FrameworkService) { }
 
@@ -19,8 +36,14 @@ export class FrameworkComponent implements OnInit {
     this.loadSavedCourses();
   }
 
+  ngAfterViewInit() {
+    this.savedCourses.paginator = this.paginator;
+    this.savedCourses.sort = this.sort;
+  }
+
   loadSavedCourses(): void {
-    this.savedCourses = this.frameworkService.getCourses();
+    const courses = this.frameworkService.getCourses();
+    this.savedCourses.data = courses;
   }
 
   onRemove(course: CourseItem): void {
