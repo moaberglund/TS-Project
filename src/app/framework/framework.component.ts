58cecc32c7fb9c3a242faba6df287d0be003e60a
moaberglund <moa.21@hotmail.com>
2024-05-21 09:28:00 +0200

    synca framework med coursetable

--- src/app/framework/framework.component.ts
+++ src/app/framework/framework.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CourseItem } from '../models/course';
+import { FrameworkService } from '../services/framework.service';
 
 @Component({
   selector: 'app-framework',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './framework.component.html',
   styleUrl: './framework.component.scss'
 })
-export class FrameworkComponent {
+export class FrameworkComponent implements OnInit {
+
+  savedCourses: CourseItem[] = [];
+
+  constructor(private frameworkService: FrameworkService) { }
+
+  ngOnInit(): void {
+    this.loadSavedCourses();
+  }
+
+  loadSavedCourses(): void {
+    this.savedCourses = this.frameworkService.getCourses();
+  }
+
+  onRemove(course: CourseItem): void {
+    this.frameworkService.removeCourse(course);
+    this.loadSavedCourses(); // Uppdatera listan efter borttagning
+  }
 
 }
