4727cb26b10eff0e56b1e0faa1ce746a7f6b37fd
moaberglund <moa.21@hotmail.com>
2024-05-22 15:46:53 +0200

    footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
